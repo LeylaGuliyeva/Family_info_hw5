4f368ac74125ec481cf2d78cc999bd1c6cb12768
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-01 00:08:00 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,29 @@
 
+
 public class Main {
     public static void main(String[] args) {
         Pet hamster=new Pet();
         Pet carrot=new Pet("Carrot","Chuky");
         Pet cat=new Pet("Cat","Kandela",2,99,new String[]{"Eating","Sleeping","Creating a mess"});
+        byte iq=100;
+        String sl[][]={{"Monday","Tuesday","Wednesday","Thursday","Friday"},{"Playing piano","Doing math","Yoga","Reading book","Watching AOT"}};
         Human Samin=new Human("Samin","Alakbarov",1982);
         Human Lala=new Human("Lala","Alakbarova",2003) ;
-        Human Eren=new Human("Eren","Alakbarov",2029,Lala,Samin);
-        String sl[][]={{"Monday","Tuesday","Wednesday","Thursday","Friday"},{"Playing piano","Doing math","Yoga","Reading book","Watching AOT"}};
-        byte iq=100;
-        Human Mikasa=new Human("Mikasa","Alakbarova",2031,iq,cat,Lala,Samin,sl);
-        Human nobody=new Human();
-        System.out.println(carrot);
-        carrot.eat();
-        carrot.foul();
-        carrot.respond();
-        Mikasa.greetPet();
-        Mikasa.describePet();
-        System.out.println(cat);
-        System.out.println(carrot);
-        System.out.println(hamster);
+        Family fami=new Family(Lala,Samin);
+        Human Eren=new Human("Eren","Alakbarov",2029,iq=100,sl,fami);
+        fami.setPet(cat);
+        fami.addChild(Eren);
         System.out.println(Samin);
         System.out.println(Lala);
+        System.out.println(cat);
         System.out.println(Eren);
-        System.out.println(Mikasa);
-        System.out.println(nobody);
+        System.out.println(fami);
+        fami.deleteChild(0);
+        System.out.println(fami);
+        System.out.println(Samin.equals(Lala));
+        System.out.println(Eren.equals(Eren));
+
+
     }
 
 }
